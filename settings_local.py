e1981370a9f75e36d664e5aa9a72652c551d0941
murlux <murlux@protonmail.com>
2020-06-08 01:30:53 +0100

    Additions to gitignore due to settings.

--- settings_local.py
+++ settings_local.py
@@ -5,7 +5,7 @@
 
 # ######### general parameters ############## #
 
-BASE_FOLDER = '/envs/playground-data/'
+BASE_FOLDER = 'path/to/your/data/folder'
 DATASET_FOLDER = '{}/data/'.format(BASE_FOLDER)
 CHARTS_FOLDER = '{}/charts/'.format(BASE_FOLDER)
 BACKTESTS_CHARTS_FOLDER = CHARTS_FOLDER + '{}/'.format('backtests')
@@ -15,32 +15,32 @@ FORWARDTESTS_CHARTS_FOLDER = CHARTS_FOLDER + '{}/'.format('forwardtests')
 
 # ########## COIN MARKET CAP PRO API KEY ########### #
 # used for some global data
-CMC_PRO_API_KEY = '770d19e3-b391-4e99-8e48-67dfcf10d508' 
+CMC_PRO_API_KEY = 'cmc_pro_api_key' 
 
 # ######### CRYPTOCOMPARE API KEY ###### #
 # default key in case of not tracking 1min dataset (44k calls/mo)
-CCAPI_KEY = 'e0a25ef4356a791930d551c667978659c56031cc2d1527aa4c8c8aee74f18a82'
+CCAPI_KEY = 'ccapi_key'
 
 # pair-specific API keys in case of tracking the 1min dataset + others
 
 # USD PAIRS #
-CCAPI_KEY_BTCUSD  = '5fc1c96274a3a9eba10d40a32bbc748a0bfc202394dd687bc2029610998017f6'
-CCAPI_KEY_ETHUSD  = '5e7ee58e47f8aaf0dbb3b099a2442339875fae236f11bb50a2381305b69c6aa2'
-CCAPI_KEY_XTZUSD  = 'd78694e68d327c6b767b1571092d0bba361e0ae7af6d5213639f2ebc708ed29e'
-CCAPI_KEY_LINKUSD = 'caba1f0da560024d4c43122114b2723cce20a877a47c6a388df4fe8de36f134e'
-CCAPI_KEY_XRPUSD  = 'e094138ed189dce1310f0928d510cc182f43ca1f5b39a669b0757031f9b90f1d'
-CCAPI_KEY_LTCUSD  = '0e4e13fc6e4cb73b6d1f17f95eb82c23a955bb58a2f8fb03c236fbf5eea229f7'
+CCAPI_KEY_BTCUSD  = 'ccapi_key_pair_1'
+CCAPI_KEY_ETHUSD  = 'ccapi_key_pair_2'
+CCAPI_KEY_XTZUSD  = 'ccapi_key_pair_3'
+CCAPI_KEY_LINKUSD = 'ccapi_key_pair_4'
+CCAPI_KEY_XRPUSD  = 'ccapi_key_pair_5'
+CCAPI_KEY_LTCUSD  = 'ccapi_key_pair_6'
 
 # BTC PAIRS #
-CCAPI_KEY_ETHBTC = 'bc03e3ad3106b6ba61d8212f95f313a555792e0400e504f1d3d772de9d8dd7ae'
-CCAPI_KEY_LTCBTC = '11b6d7e06f59f6bbee70e19d277c99307fb3a0c21b047c76f96b64990efa9991'
-CCAPI_KEY_TRXBTC = '60210783283e735e3d897c5511b4a15aabb42616dff83ae2d73e36544de94462'
-CCAPI_KEY_XRPBTC = '052e4236632051777a00122609e5d7ec3570074f90ddad863ca9399aef722cca'
+CCAPI_KEY_ETHBTC = 'ccapi_key_pair_7'
+CCAPI_KEY_LTCBTC = 'ccapi_key_pair_8'
+CCAPI_KEY_TRXBTC = 'ccapi_key_pair_9'
+CCAPI_KEY_XRPBTC = 'ccapi_key_pair_10'
 
 
 # ######### TWITTER ######### #
-CONSUMER_SECRET = 'lkcRgPmbf6Yk9StOdJIDzKwCOMBC3yjWtVmlHuqTOxESzzPEGq'
-CONSUMER_KEY = 'MSqgDlAlPQvuQIj1sLpgln0V4'
+CONSUMER_SECRET = 'twitter_consumer_secret'
+CONSUMER_KEY = 'twitter_consumer_key'
 
-ACCESS_TOKEN = '1216046823066030080-18xeUQh8GeomlsVco52yz4HlNnosp4'
-ACCESS_TOKEN_SECRET = 'hrIKiSIRdUPQ7B1sZtOsBkssDnyWzmSrnAgCzDq8yfeEA'
+ACCESS_TOKEN = 'twitter_access_token'
+ACCESS_TOKEN_SECRET = 'twitter_access_token_secret'
